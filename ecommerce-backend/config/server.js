f06c986aea41ceeb234988647cdedef8a3fb64db
NasirHaidari <nasir.haidri@htomail.com>
2021-05-31 23:09:28 +0200

    backEnd is done now

---
+++ ecommerce-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '09cdea9d84649b468399c243bd7c56c5'),
+    },
+  },
+});
---
+++ ecommerce-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ff51aced-a895-440f-beaa-ae6b1bf4652d'
+};
